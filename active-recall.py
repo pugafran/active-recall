8e678c184ded82500dd402e1063b5a762ad6e7f6
pugafran <franciscplgi36@gmail.com>
2023-09-03 21:54:17 +0200

    03-09-23

--- active-recall.py
+++ active-recall.py
@@ -3,7 +3,7 @@ from tkinter import filedialog, simpledialog
 import pandas as pd
 import random
 import unidecode
-
+from fuzzywuzzy import fuzz
 
 class FlashCardApp:
     def __init__(self, root):
@@ -19,17 +19,17 @@ class FlashCardApp:
         self.questions_limit = 0
         self.failed_questions = []
 
-        self.question_counter_label = tk.Label(root, text="", font=("Arial", 12), bg='lightblue', fg='darkred')
+        self.question_counter_label = tk.Label(root, text="", font=("Arial", 12), bg='lightblue', fg='darkred', wraplength=550)
         self.question_counter_label.pack(pady=10)
 
-        self.question_label = tk.Label(root, text="", font=("Arial", 24), bg='lightblue', fg='darkblue')
+        self.question_label = tk.Text(root, height=5, font=("Arial", 24), bg='lightblue', fg='darkblue', wrap='word')
         self.question_label.pack(pady=10)
 
         self.answer_entry = tk.Entry(root, font=("Arial", 20))
         self.answer_entry.pack(pady=10)
         self.answer_entry.bind('<Return>', self.check_answer)
 
-        self.stats_label = tk.Label(root, text="", font=("Arial", 20), bg='lightblue', fg='darkgreen')
+        self.stats_label = tk.Label(root, text="", font=("Arial", 20), bg='lightblue', fg='darkgreen', wraplength=550)
         self.stats_label.pack(pady=10)
 
         self.buttons_frame = tk.Frame(root, bg='lightblue')
@@ -39,11 +39,6 @@ class FlashCardApp:
                                             bg='darkgreen', fg='white', width=20)
         self.show_answer_button.pack(side='left', padx=5)
 
-       # self.next_button = tk.Button(self.buttons_frame, text="Siguiente", command=self.show_next_card, bg='darkblue',
-       #                             fg='white', width=20)
-       #self.next_button.pack(side='right', padx=5)
-
-        # Esperar hasta que la interfaz esté completamente inicializada antes de abrir la ventana de diálogo
         root.after(100, self.start_quiz)
 
     def start_quiz(self):
@@ -60,27 +55,35 @@ class FlashCardApp:
         if self.questions_seen < self.questions_limit:
             self.card_index = random.randint(0, len(self.data) - 1)
             question = self.data.loc[self.card_index, 'Pregunta']
-            self.question_label.config(text=question)
+            self.question_label.delete(1.0, tk.END)
+            self.question_label.insert(tk.END, question)
             self.answer_entry.delete(0, 'end')
             self.questions_seen += 1
             self.update_stats()
             self.question_counter_label.config(text=f"Pregunta {self.questions_seen}")
         else:
-            self.question_label.config(text="Quiz finalizado")
+            self.question_label.delete(1.0, tk.END)
+            self.question_label.insert(tk.END, "Quiz finalizado")
             self.answer_entry.pack_forget()
             self.buttons_frame.pack_forget()
             self.show_failed_questions()
 
     def check_answer(self, event=None):
         user_answer = self.answer_entry.get().lower()
-        correct_answer = unidecode.unidecode(self.data.loc[self.card_index, 'Respuesta'].lower())
+        correct_answer = unidecode.unidecode(str(self.data.loc[self.card_index, 'Respuesta']).lower())
         user_answer = unidecode.unidecode(user_answer)
-        if user_answer == correct_answer:
+
+        if fuzz.ratio(user_answer, correct_answer) >= 55:
             self.questions_correct += 1
+            self.show_next_card()
         else:
             self.failed_questions.append(self.data.loc[self.card_index])
-        self.show_next_card()
-        self.update_stats()
+            self.show_answer_button.configure(text="Siguiente Pregunta", command=self.show_next_card)
+            self.answer_entry.config(state='disabled')
+            self.answer_entry.delete(0, 'end')
+            self.answer_entry.config(state='normal')
+            self.answer_entry.focus_set()
+            self.question_label.insert(tk.END, f"\n\nRespuesta Correcta: {self.data.loc[self.card_index, 'Respuesta']}")
 
     def update_stats(self):
         stats_text = f"Vistas: {self.questions_seen} / Correctas: {self.questions_correct}"
@@ -90,9 +93,11 @@ class FlashCardApp:
         if self.failed_questions:
             failed_text = "Preguntas fallidas:\n"
             for question in self.failed_questions:
-                failed_text += f"Pregunta: {question['Pregunta']} - Respuesta correcta: {question['Respuesta']}\n"
-            self.failed_questions_label = tk.Label(self.root, text=failed_text, font=("Arial", 20), bg='lightblue', fg='darkred')
+                failed_text += f"Pregunta: {question['Pregunta']} - Respuesta correcta: {question['Respuesta']}\n\n"  # Agregamos espacio adicional
+            self.failed_questions_label = tk.Text(self.root, height=15, font=("Arial", 20), bg='lightblue', fg='darkred')  # Aumentamos la altura
             self.failed_questions_label.pack(pady=10)
+            self.failed_questions_label.insert(tk.END, failed_text)
+            self.failed_questions_label.config(state='disabled')  # Deshabilita la edición
 
 
 root = tk.Tk()
